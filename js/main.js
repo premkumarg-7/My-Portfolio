0600f61c8aef28a95a0f9fe976c4cc7bd604fc8d
premkumarg-7 <premkumarg142@gmail.com>
2024-04-03 14:59:10 +0530

    portfolio editted

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ var typed=new Typed(".text",{
     loop:true
 
 })
+
+document.getElementById('contact-form') .addEventListener('submit',function (event){
+    event.preventDefault();
+    var data = new FormData(event.target);
+    fetch("https://formsubmit.co/el/zanopo",{
+        method:"POST",
+        body:data,
+    }).then(response =>{
+        if (response.ok) {
+            alert('Success');
+        } else{
+            alert('Error');
+        }
+    });
+    document.getElementById('contact-form').reset();
+});
